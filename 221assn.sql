bd4e70169143f8048c2728c15fdd71680550deef
aaron6347 <49943930+aaron6347@users.noreply.github.com>
2019-12-08 23:40:16 +0800

    a2

--- 221assn.sql
+++ 221assn.sql
@@ -176,24 +176,24 @@ Deduction_name VARCHAR(40) NOT NULL,
 Deduction_rate NUMBER(4,2) NOT NULL
 );
 CREATE TABLE PAYSLIP(
-Payslip_num NUMBER PRIMARY KEY,
+Payslip_num VARCHAR(10) PRIMARY KEY,
 Employee_id VARCHAR(10) NOT NULL,
 Pay_date DATE NOT NULL,
-Total_allowance NUMBER NOT NULL,
-Total_incentive NUMBER NOT NULL,
-Total_deduction NUMBER NOT NULL,
-Total_salary NUMBER NOT NULL,
+Total_allowance NUMBER,
+Total_incentive NUMBER,
+Total_deduction NUMBER,
+Total_salary NUMBER DEFAULT(1),
 FOREIGN KEY (Employee_id) REFERENCES EMPLOYEE(Employee_id)
 );
 CREATE TABLE PAYSLIP_ALLOWANCE(
-Payslip_num NUMBER,
+Payslip_num VARCHAR(10),
 Allowance_id VARCHAR(6),
 PRIMARY KEY (Payslip_num, Allowance_id),
 FOREIGN KEY (Payslip_num) REFERENCES PAYSLIP(Payslip_num),
 FOREIGN KEY (Allowance_id) REFERENCES ALLOWANCE(Allowance_id)
 ); 
 CREATE TABLE PAYSLIP_INCENTIVE(
-Payslip_num NUMBER,
+Payslip_num VARCHAR(10),
 Incentive_id VARCHAR(6),
 Incentive_amount NUMBER(6,2),
 PRIMARY KEY (Payslip_num, Incentive_id),
@@ -201,7 +201,7 @@ FOREIGN KEY (Payslip_num) REFERENCES PAYSLIP(Payslip_num),
 FOREIGN KEY (Incentive_id) REFERENCES INCENTIVE(Incentive_id)
 );
 CREATE TABLE PAYSLIP_DEDUCTION(
-Payslip_num NUMBER,
+Payslip_num VARCHAR(10),
 Deduction_id VARCHAR(6),
 PRIMARY KEY (Payslip_num, Deduction_id),
 FOREIGN KEY (Payslip_num) REFERENCES PAYSLIP(Payslip_num),
